8acb2995ce02cd97acb75b18374811dae6a9051d
farzan alaei <farzan.70@gmail.com>
2024-08-21 21:49:09 +0330

    add template and add debug toolbar

--- storefront/urls.py
+++ storefront/urls.py
@@ -22,4 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("playground/", include("playground.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
