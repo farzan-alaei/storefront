739d92deeccdfd9eae17ecbf4ff99ac49236bdf5
farzan alaei <farzan.70@gmail.com>
2024-08-22 13:19:43 +0330

    add limit to query

--- playground/views.py
+++ playground/views.py
@@ -4,5 +4,5 @@ from store.models import Product
 
 
 def say_hello(request):
-    queryset = Product.objects.order_by("title")
+    queryset = Product.objects.order_by("title")[:5]
     return render(request, 'hello.html', {'name': 'Mosh', 'page_obj': list(queryset)})
