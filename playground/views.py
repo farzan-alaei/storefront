68493f56c0b0fe04b9e8a2964a7db36559d45427
farzan alaei <farzan.70@gmail.com>
2024-08-22 18:39:01 +0330

    add transaction atomic to query

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from store.models import Collection, Product, Cart, CartItem
+from django.db import transaction
+from store.models import Order, OrderItem
 
 
 def say_hello(request):
-    cart = Cart.objects.filter(pk=1).delete()
+    with transaction.atomic():
+        order = Order()
+        order.customer_id = 1
+        order.save()
+
+        item = OrderItem()
+        item.order = order
+        item.product_id = 1
+        item.quantity = 1
+        item.unit_price = 10
+        item.save()
 
     return render(request, 'hello.html', {'name': 'Mosh'})
