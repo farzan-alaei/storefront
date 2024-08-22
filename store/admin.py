1ae8f90725b039b78542d68733afdc2ea60d5126
farzan alaei <farzan.70@gmail.com>
2024-08-23 00:18:58 +0330

    adding customization to admin model

--- store/admin.py
+++ store/admin.py
@@ -2,13 +2,19 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Collection)
-admin.site.register(models.Product)
-admin.site.register(models.Promotion)
-admin.site.register(models.Cart)
-admin.site.register(models.CartItem)
-admin.site.register(models.Customer)
-admin.site.register(models.Order)
-admin.site.register(models.OrderItem)
-admin.site.register(models.Address)
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
+    list_per_page = 10
 
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
+
+
+admin.site.register(models.Collection)
