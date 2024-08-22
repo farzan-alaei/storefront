0dfc336654ab318c904c2aaa0f0c8b3dcf65d0c8
farzan alaei <farzan.70@gmail.com>
2024-08-22 18:21:40 +0330

    add delete method to view

--- playground/views.py
+++ playground/views.py
@@ -3,5 +3,5 @@ from store.models import Collection, Product
 
 
 def say_hello(request):
-    Collection.objects.filter(pk=11).update(featured_product=None)
+    Collection.objects.filter(pk=11).delete()
     return render(request, 'hello.html', {'name': 'Mosh'})
