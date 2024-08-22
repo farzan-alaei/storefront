b35b143817304b5a2124ffb3b8b643852106f428
farzan alaei <farzan.70@gmail.com>
2024-08-22 13:36:39 +0330

    add to model filter in queryset, select field in query

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.db.models import Q, F
-from store.models import Product
+from store.models import OrderItem, Product
 
 
 def say_hello(request):
-    queryset = Product.objects.order_by("title")[:5]
+    queryset = Product.objects.filter(
+        id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
     return render(request, 'hello.html', {'name': 'Mosh', 'page_obj': list(queryset)})
