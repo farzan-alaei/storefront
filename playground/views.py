3cf67fffe31fb7736c1990eead13e42142348266
farzan alaei <farzan.70@gmail.com>
2024-08-22 18:19:02 +0330

    add update method to view

--- playground/views.py
+++ playground/views.py
@@ -3,8 +3,5 @@ from store.models import Collection, Product
 
 
 def say_hello(request):
-    collection = Collection()
-    collection.title = "video game"
-    collection.featured_product = Product(pk=1)
-    collection.save()
+    Collection.objects.filter(pk=11).update(featured_product=None)
     return render(request, 'hello.html', {'name': 'Mosh'})
