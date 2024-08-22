2552d464953c68f16bc95796380fe4abb53c2975
farzan alaei <farzan.70@gmail.com>
2024-08-22 16:53:48 +0330

    add db func to query

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.db.models import Value
-from store.models import Product
+from django.db.models import Value, Func, F
+from django.db.models.functions import Concat
+from store.models import Customer
 
 
 def say_hello(request):
-    queryset = Product.objects.annotate(is_new=Value(True))
+    queryset = Customer.objects.annotate(full_name=Func(
+        F('first_name'), Value(' '), F('last_name'), function='CONCAT'))
+    
+    query = Customer.objects.annotate(Concat=Concat('first_name', Value(' '), 'last_name'))
 
-    return render(request, 'hello.html', {'name': 'Mosh', 'page_obj': queryset})
+    return render(request, 'hello.html', {'name': 'Mosh', 'page_obj': query})
