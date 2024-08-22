f6ea4fb0ab2c677858e278ee51ed68bd37565f2c
farzan alaei <farzan.70@gmail.com>
2024-08-22 17:00:12 +0330

    add orders count for each costumer to query

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from django.db.models import Value, Func, F
+from django.db.models import Value, Func, F, Count
 from django.db.models.functions import Concat
 from store.models import Customer
 
 
 def say_hello(request):
-    queryset = Customer.objects.annotate(full_name=Func(
-        F('first_name'), Value(' '), F('last_name'), function='CONCAT'))
-    
-    query = Customer.objects.annotate(Concat=Concat('first_name', Value(' '), 'last_name'))
+    queryset = Customer.objects.annotate(orders_count=Count('order'))
 
-    return render(request, 'hello.html', {'name': 'Mosh', 'page_obj': query})
+    return render(request, 'hello.html', {'name': 'Mosh', 'page_obj': queryset})
