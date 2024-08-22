672b487303a06f188ed8dd6b856670f81e889e3e
farzan alaei <farzan.70@gmail.com>
2024-08-22 13:56:02 +0330

    add select and prefetch related to query

--- playground/views.py
+++ playground/views.py
@@ -4,6 +4,6 @@ from store.models import OrderItem, Product
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(
-        id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    queryset = Product.objects.prefetch_related(
+        'promotions').select_related('collection').all()
     return render(request, 'hello.html', {'name': 'Mosh', 'page_obj': list(queryset)})
