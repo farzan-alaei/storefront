934de717e5232029fc42ac3e95ab4d941a884cf3
farzan alaei <farzan.70@gmail.com>
2024-08-23 00:25:24 +0330

    add computing method to product admin

--- store/admin.py
+++ store/admin.py
@@ -4,9 +4,15 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventroy_status']
     list_editable = ['unit_price']
     list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventroy_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
 
 
 @admin.register(models.Customer)
