e1e4a2347203fd360e9fc3e7491304ca322617de
farzan alaei <farzan.70@gmail.com>
2024-08-22 14:16:56 +0330

    add complex query to view

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.db.models import Q, F
-from store.models import OrderItem, Product
+from store.models import Order, Product
 
 
 def say_hello(request):
-    queryset = Product.objects.prefetch_related(
-        'promotions').select_related('collection').all()
+    queryset = Order.objects.select_related('customer').prefetch_related(
+        'orderitem_set__product').order_by('-placed_at')[:5]
     return render(request, 'hello.html', {'name': 'Mosh', 'page_obj': list(queryset)})
