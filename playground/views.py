3b64b0e0d03f6698584273e20f2f2d41cd23eb05
farzan alaei <farzan.70@gmail.com>
2024-08-22 17:07:12 +0330

    add expressionwrapperr to query

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.db.models import Value, Func, F, Count
+from django.db.models import Value, Func, F, Count, ExpressionWrapper, DecimalField
 from django.db.models.functions import Concat
-from store.models import Customer
+from store.models import Product
 
 
 def say_hello(request):
-    queryset = Customer.objects.annotate(orders_count=Count('order'))
+    discounted_price = ExpressionWrapper(
+        F('unit_price') * 0.8, output_field=DecimalField())
+    queryset = Product.objects.annotate(discounted_price=discounted_price)
 
     return render(request, 'hello.html', {'name': 'Mosh', 'page_obj': queryset})
