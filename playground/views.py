7823e270fa144b1c482b8303b83eb61fa05d5d16
farzan alaei <farzan.70@gmail.com>
2024-08-22 17:35:26 +0330

    add annotate etc to query

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.db.models import Value, Func, F, Count, ExpressionWrapper, DecimalField
+from django.db.models import Value, Func, F, Count, Max, Sum
 from django.db.models.functions import Concat
 from store.models import Product
 
 
 def say_hello(request):
-    discounted_price = ExpressionWrapper(
-        F('unit_price') * 0.8, output_field=DecimalField())
-    queryset = Product.objects.annotate(discounted_price=discounted_price)
+    queryset = Product.objects.annotate(
+        total_sales=Sum('orderitem__quantity')
+    ).order_by('-total_sales')[:5]
 
     return render(request, 'hello.html', {'name': 'Mosh', 'page_obj': queryset})
