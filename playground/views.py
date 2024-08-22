84123b984a88f0c9127bc13e3a4dc0f56c6ff745
farzan alaei <farzan.70@gmail.com>
2024-08-22 12:33:27 +0330

    use F Object in query

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.db.models import Q
+from django.db.models import Q, F
 from store.models import Product
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(
-        Q(unit_price__lt=10) & ~Q(inventory__lt=10))
+    queryset = Product.objects.filter(inventory=F('collection__id'))
     return render(request, 'hello.html', {'name': 'Mosh', 'page_obj': list(queryset)})
