889ad8c0a6f0f0094ebaed8fff0d0528d732bdb1
farzan alaei <farzan.70@gmail.com>
2024-08-23 18:11:33 +0330

    customizing form in admin panel

--- store/admin.py
+++ store/admin.py
@@ -24,6 +24,8 @@ class InventoryFilter(admin.SimpleListFilter):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {'slug': ['title']}
     actions = ['clear_inventory']
     list_display = ['title', 'unit_price',
                     'inventroy_status', 'collection_title']
@@ -31,6 +33,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ['unit_price']
     list_filter = ['collection', 'last_update', InventoryFilter]
     list_select_related = ['collection']
+    search_fields = ['title']
 
     def collection_title(self, product):
         return product.collection.title
@@ -78,12 +81,17 @@ class CustomerAdmin(admin.ModelAdmin):
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['customer']
     list_display = ['id', 'placed_at', 'customer']
+    list_per_page = 10
+
 
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['featured_product']
     list_display = ['title', 'products_count']
+    search_fields = ['title']
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
