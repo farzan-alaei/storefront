84bf03b80dd9250733de81d8e7c7bf6e998eb7d2
farzan alaei <farzan.70@gmail.com>
2024-08-28 17:37:17 +0330

    add cart item to cart and method field to serializer of cart and cart item for calculate total price of cart and cart item

--- store/views.py
+++ store/views.py
@@ -56,5 +56,5 @@ class ReviewViewSet(ModelViewSet):
 
 
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
-    queryset = Cart.objects.all()
+    queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
