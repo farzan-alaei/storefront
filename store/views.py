8057783128ea339e18b1144e5d21b1305118ae49
farzan alaei <farzan.70@gmail.com>
2024-08-29 16:26:08 +0330

    building the profile api

--- store/views.py
+++ store/views.py
@@ -2,13 +2,13 @@ from django.db.models.aggregates import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
-from .models import Product, Collection, OrderItem, Review, Cart, CartItem
+from .models import Product, Collection, OrderItem, Review, Cart, CartItem, Customer
 from .serializers import (ProductSerializer, CollectionSerializer, ReviewSerializer,
                           CartSerializer, CartItemSerializer, AddCartItemSerializer,
-                          UpdateCartItemSerializer,)
+                          UpdateCartItemSerializer, CustomerSerializer)
 from .pagination import DefaultPagination
 from .filters import ProductFilter
 # Create your views here.
@@ -83,3 +83,8 @@ class CartItemViewSet(ModelViewSet):
         return CartItem.objects \
             .select_related('product') \
             .filter(cart_id=self.kwargs['cart_pk'])
+
+
+class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer 
