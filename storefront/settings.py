a8f50a52aa02073fdeced142ff63aad3d1ba57cc
farzan alaei <farzan.70@gmail.com>
2024-08-29 14:24:03 +0330

    change store_custom name to core, and add custom user model (abstract) for extending the user model

--- storefront/settings.py
+++ storefront/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'playground',
     'debug_toolbar',
     'store',
-    'store_custom',
     'tags',
     'likes',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -145,3 +145,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False
 }
+
+# User model for authentication
+AUTH_USER_MODEL = 'core.User'
