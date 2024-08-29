861096d096acd35fbc942bc1034c5027be6dbe03
farzan alaei <farzan.70@gmail.com>
2024-08-29 08:31:57 +0330

    add deleting carts to view

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.db.models.aggregates import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Product, Collection, OrderItem, Review, Cart
@@ -55,6 +55,10 @@ class ReviewViewSet(ModelViewSet):
         return {'product_id': self.kwargs['product_pk']}
 
 
-class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+class CartViewSet(CreateModelMixin,
+                  RetrieveModelMixin,
+                  DestroyModelMixin,
+                  GenericViewSet):
+
     queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
