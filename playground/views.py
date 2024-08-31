bd7f12a64d9c7f40759669f588df0ab56a90c1a6
farzan <farzan.70@gmial.com>
2024-08-31 15:33:42 +0330

    sending email to user

--- playground/views.py
+++ playground/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def say_hello(request):
     try:
-        mail_admins('subject', 'message', html_message='message')
+        send_mail('subject', 'message', 'info@farzan.com', ['farzan@farzan.com'])
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name': 'Mosh'})
