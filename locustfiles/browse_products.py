aed64a8e256fdd2bdf3cbb336816bcda85dea641
farzan <farzan.70@gmial.com>
2024-09-13 12:48:53 +0330

    fix performance testing locust views

--- locustfiles/browse_products.py
+++ locustfiles/browse_products.py
@@ -10,7 +10,7 @@ class WebsiteUser(HttpUser):
         print("View Products")
         collection_id = randint(2, 6)
         self.client.get(
-            f"/store/products/?collection_id={collection_id}/", name="/store/products"
+            f"/store/products/?collection_id={collection_id}", name="/store/products"
         )
 
     @task
